25737f7438622eff3dd50756c4a68390cf29051c
niki <nikym6@gmail.com>
2019-04-04 17:25:08 +0200

    first commit

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -7,18 +7,17 @@ import { products } from '../products';
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent{
+export class ProductListComponent {
   products = products;
 
   share() {
     window.alert('The product has been shared!');
   }
 
-  onNotify(){
+  onNotify() {
     window.alert('You will be notified when the product goes on sale');
   }
 }
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
